9f7517f4d49436a15de9e5232c3cc9ef57c70577
leroyn <leroy.nguyen04@gmail.com>
2024-11-07 03:03:30 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// DOM elements
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+let themeModeToggleBtn = document.getElementById("theme-toggle-btn")
+
+// Conversion factors
+const meterToFeet = 3.28084
+const literToGallon = 0.264172
+const kiloToPound = 2.20462
+const feetToMeter = 0.3048
+const gallonToLiter = 3.78541
+const poundToKilo = 0.453592
+
+// Event listener for conversion button
+convertBtn.addEventListener("click", function () {
+  let baseValue = parseFloat(inputEl.value) // Parse input value as a float
+
+  // Update length conversion
+  lengthEl.textContent = `${baseValue} meter = ${(
+    baseValue * meterToFeet
+  ).toFixed(4)} feet | ${baseValue} feet = ${(baseValue * feetToMeter).toFixed(
+    4
+  )} meter`
+
+  // Update volume conversion
+  volumeEl.textContent = `${baseValue} liter = ${(
+    baseValue * literToGallon
+  ).toFixed(4)} gallon | ${baseValue} gallon = ${(
+    baseValue * gallonToLiter
+  ).toFixed(4)} liter`
+
+  // Update mass conversion
+  massEl.textContent = `${baseValue} kilo = ${(baseValue * kiloToPound).toFixed(
+    4
+  )} pounds | ${baseValue} pounds = ${(baseValue * poundToKilo).toFixed(
+    4
+  )} kilo`
+})
+
+// Event listener for theme toggle button
+themeModeToggleBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode") // Toggle dark mode class
+
+  // Update button text based on current theme
+  themeModeToggleBtn.textContent = document.body.classList.contains("dark-mode")
+    ? "☀️"
+    : "🌙"
+})
